c50c14ad24b7ad435aa581afd4545efe37ec523c
msc1203 <matheusholambra@gmail.com>
2024-09-02 19:32:00 -0300

    full code working

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ Configurando o python:
 cd libfreenect/wrappers/python
 
 comentar tudo que tinha a ver com o cython version no setup.py:
+pip3 install Cython
 ![e72df144-f80e-4c40-9b18-4941e0c5adb7](https://github.com/user-attachments/assets/54b6aa04-03e6-4e45-8d20-829e7ea32b8a)
 
 sudo python3 setup.py build_ext --inplace
--- full_code.py
+++ full_code.py
@@ -3,33 +3,34 @@ import threading
 import random
 import numpy as np
 import freenect
-import RPi.GPIO as GPIO #pip3 install RPi.GPIO
-
-# Configurações GPIO
-GPIO.setmode(GPIO.BCM)
-# Motor Esquerdo
-IN1 = 17  
-IN2 = 27
-# Motor Direito
-IN3 = 23  
-IN4 = 24
-# PWM's
-ENA = 22  # PWM para Motor Esquerdo
-ENB = 25  # PWM para Motor Direito
-
-# Configurar os pinos como saída
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.setup(IN4, GPIO.OUT)
-GPIO.setup(ENA, GPIO.OUT)
-GPIO.setup(ENB, GPIO.OUT)
-
-# Configurar PWM
-pwm_a = GPIO.PWM(ENA, 1000)  # 1kHz para motor esquerdo
-pwm_b = GPIO.PWM(ENB, 1000)  # 1kHz para motor direito
-pwm_a.start(25)  #duty cycle
-pwm_b.start(25)  #duty cycle
+import cv2 as cv
+#import RPi.GPIO as GPIO #pip3 install RPi.GPIO
+#
+## Configurações GPIO
+#GPIO.setmode(GPIO.BCM)
+## Motor Esquerdo
+#IN1 = 17  
+#IN2 = 27
+## Motor Direito
+#IN3 = 23  
+#IN4 = 24
+## PWM's
+#ENA = 22  # PWM para Motor Esquerdo
+#ENB = 25  # PWM para Motor Direito
+#
+## Configurar os pinos como saída
+#GPIO.setup(IN1, GPIO.OUT)
+#GPIO.setup(IN2, GPIO.OUT)
+#GPIO.setup(IN3, GPIO.OUT)
+#GPIO.setup(IN4, GPIO.OUT)
+#GPIO.setup(ENA, GPIO.OUT)
+#GPIO.setup(ENB, GPIO.OUT)
+#
+## Configurar PWM
+#pwm_a = GPIO.PWM(ENA, 1000)  # 1kHz para motor esquerdo
+#pwm_b = GPIO.PWM(ENB, 1000)  # 1kHz para motor direito
+#pwm_a.start(25)  #duty cycle
+#pwm_b.start(25)  #duty cycle
 
 def calcular_media(vetor):
     largura = len(vetor)
@@ -38,120 +39,128 @@ def calcular_media(vetor):
     return media
 
 def girar_esquerda():
+    #while not stop_event.is_set():
     print("Girando a roda esquerda")
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.HIGH)
+    #GPIO.output(IN1, GPIO.HIGH)
+    #GPIO.output(IN2, GPIO.LOW)
+    #GPIO.output(IN3, GPIO.LOW)
+    #GPIO.output(IN4, GPIO.HIGH)
     time.sleep(4)
     
 
 def girar_direita():
+    #while not stop_event.is_set():
     print("Girando a roda direita")
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.HIGH)
-    GPIO.output(IN3, GPIO.HIGH)
-    GPIO.output(IN4, GPIO.LOW)
+    #GPIO.output(IN1, GPIO.LOW)
+    #GPIO.output(IN2, GPIO.HIGH)
+    #GPIO.output(IN3, GPIO.HIGH)
+    #GPIO.output(IN4, GPIO.LOW)
     time.sleep(4)
 
-def ir_pra_frente(stop_event):
-    while not stop_event.is_set():
-        print("Indo para frente")
-        GPIO.output(IN1, GPIO.HIGH)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.HIGH)
-        GPIO.output(IN4, GPIO.LOW)
-        time.sleep(4)
+def ir_pra_frente():
+    #while not stop_event.is_set():
+    print("Indo para frente")
+    #GPIO.output(IN1, GPIO.HIGH)
+    #GPIO.output(IN2, GPIO.LOW)
+    #GPIO.output(IN3, GPIO.HIGH)
+    #GPIO.output(IN4, GPIO.LOW)
+    time.sleep(4)
 
-def analisar_vetor(vetor, limite_central, limite_lateral, intervalo, stop_event):
-    while not stop_event.is_set():
-        tamanho = len(vetor)
-        if tamanho < 3:
-            print("Vetor muito pequeno para análise")
-            break
+def parar():
+    #while not stop_event.is_set():
+    print("Parando motores")
+    #GPIO.output(IN1, GPIO.LOW)
+    #GPIO.output(IN2, GPIO.LOW)
+    #GPIO.output(IN3, GPIO.LOW)
+    #GPIO.output(IN4, GPIO.LOW)
+    time.sleep(4)
 
-        tercio = tamanho // 3
-
-        parte_esquerda = vetor[:tercio]
-        parte_central = vetor[tercio:2*tercio]
-        parte_direita = vetor[2*tercio:]
-
-        media_esquerda = calcular_media(parte_esquerda)
-        media_central = calcular_media(parte_central)
-        media_direita = calcular_media(parte_direita)
-
-        print(f"Média Esquerda: {media_esquerda}, Média Central: {media_central}, Média Direita: {media_direita}")
-
-        if media_central > limite_central:
-            print("Caminho livre a frente")
-            if media_direita > media_esquerda:
-                if media_esquerda < limite_lateral:
-                    girar_esquerda()
-                else:
-                    print("Continue em frente")
-            elif media_direita < media_esquerda:
-                if media_direita < limite_lateral:
-                    girar_direita()
-                else:
-                    print("Continue em frente")
-        else:
-            print("Obstáculo a frente")
-            if media_direita > media_esquerda:
+def analisar_vetor(vetor, limite_central, limite_lateral):
+    #while not stop_event.is_set():
+    tamanho = len(vetor)
+    if tamanho < 3:
+        print("Vetor muito pequeno para análise")
+        return 0
+    #
+    tercio = tamanho // 3
+    #
+    parte_esquerda = vetor[:tercio]
+    parte_central = vetor[tercio:2*tercio]
+    parte_direita = vetor[2*tercio:]
+    #
+    media_esquerda = calcular_media(parte_esquerda)
+    media_central = calcular_media(parte_central)
+    media_direita = calcular_media(parte_direita)
+    #
+    print(f"Média Esquerda: {media_esquerda}, Média Central: {media_central}, Média Direita: {media_direita}")
+    #
+    if media_central > limite_central:
+        print("Caminho livre a frente")
+        if media_direita > media_esquerda:
+            if media_esquerda < limite_lateral:
                 girar_esquerda()
             else:
+                print("Continue em frente")
+        elif media_direita < media_esquerda:
+            if media_direita < limite_lateral:
                 girar_direita()
-
-        time.sleep(intervalo)
-
-def atualizar_vetor(vetor, stop_event, intervalo):
-    while not stop_event.is_set():
-        for i in range(len(vetor)):
-            vetor[i] = random.randint(20, 60)
-        time.sleep(intervalo)
+            else:
+                print("Continue em frente")
+    else:
+        print("Obstáculo a frente")
+        if media_direita < limite_lateral:
+            if media_esquerda < limite_lateral:
+                print("Para")
+                parar()
+            
+        elif media_esquerda > media_direita:
+            girar_direita()
+        else:
+            girar_esquerda()
 
 def monitorar_tecla(stop_event):
     input("Pressione 'Enter' para parar...")
     stop_event.set()
-    pwm_a.stop()
-    pwm_b.stop()
-    GPIO.cleanup()
+    #pwm_a.stop()
+    #pwm_b.stop()
 
-def get_vector():
-    depth, _ = freenect.sync_get_depth()
-    vector = depth * 0.001
-    #vector = np.random.randint(20, 150, (240, 320), dtype=np.uint8)
-    matriz_sem_colunas = vector[:, 1:-1]
+def get_vector(vector):
+    matriz_sem_colunas = vector[:, 2:-2]
     media_colunas = np.mean(matriz_sem_colunas, axis=0)
-    vector = media_colunas.reshape(len(media_colunas), 1)
-    return vector
+    avg_data = media_colunas.reshape(len(media_colunas), 1)
+    
+    return avg_data
 
+def get_images():
+    depth, _ = freenect.sync_get_depth()
+    array,_ = freenect.sync_get_video()
+    array = cv.cvtColor(array,cv.COLOR_RGB2BGR)
+
+    return depth, array
+
+def depth_config():
+    depth, array = get_images()
+    depth_scaled = np.uint8(depth / 2048 * 255)
+    depth_scaled = depth_scaled[240:, :]
+
+    return depth_scaled, array
+        
 def main():
-    limite_central = 30
-    limite_lateral = 30
-    intervalo = 5
+    
+    
+    while True:
+        limite_central = 80
+        limite_lateral = 80
 
-    vetor_distancias = get_vector()
+        depth_data, raw_data = depth_config()
+        depth_colored = cv.applyColorMap(depth_data, cv.COLORMAP_JET)
+        cv.imshow('Depth Image', depth_colored)
+        new_depth_data = get_vector(depth_data)
+        analisar_vetor(new_depth_data,limite_central, limite_lateral )
 
-    stop_event = threading.Event()
-
-    thread_ir_pra_frente = threading.Thread(target=ir_pra_frente, args=(stop_event,))
-    thread_ir_pra_frente.daemon = True
-    thread_ir_pra_frente.start()
-
-    thread_analisar_vetor = threading.Thread(target=analisar_vetor, args=(vetor_distancias, limite_central, limite_lateral, intervalo, stop_event))
-    thread_analisar_vetor.start()
-
-    thread_atualizar_vetor = threading.Thread(target=atualizar_vetor, args=(vetor_distancias, stop_event, intervalo))
-    thread_atualizar_vetor.start()
-
-    thread_monitorar_tecla = threading.Thread(target=monitorar_tecla, args=(stop_event,))
-    thread_monitorar_tecla.start()
-
-    thread_analisar_vetor.join()
-    thread_atualizar_vetor.join()
-
-    if stop_event.is_set():
-        print("Encerrando programa...")
+        if cv.waitKey(1) & 0xFF == ord("q"):
+            parar()
+            break
 
 if __name__ == "__main__":
     main()
